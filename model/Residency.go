1b928689481e8b92e071719c3318d1baefbb6d69
Luiz Ribas <lhs.ribas@gmail.com>
2021-05-06 10:13:56 -0300

    add new fiedls

--- model/Residency.go
+++ model/Residency.go
@@ -8,10 +8,11 @@ import (
 
 // Residency - Struct a resdency that have a date predction to start
 type Residency struct {
-	ID          bson.ObjectId `bson:"_Id"          json:"Id"`
-	Name        string        `bson:"name"         json:"name"`
-	SalesKey    string        `bson:"salesKey"     json:"salesKey"`
-	Description string        `bson:"description"  json:"description"`
-	Prediction  time.Time     `bson:"prediction"   json:"prediction"`
-	CustomerID  string        `bson:"customerId"   json:"customerId"`
+	ID          bson.ObjectId `bson:"_Id"          	json:"Id"`
+	Name        string        `bson:"name"         	json:"name"`
+	SalesKey    string        `bson:"salesKey"     	json:"salesKey"`
+	Description string        `bson:"description"  	json:"description"`
+	Prediction  time.Time     `bson:"prediction"   	json:"prediction"`
+	CustomerID  string        `bson:"customerId"   	json:"customerId"`
+	IsConfirmed bool          `bson:"isConfirmed"  	json:"isConfirmed"`
 }
--- model/StaffMember.go
+++ model/StaffMember.go
@@ -6,10 +6,10 @@ import (
 
 // Team - Struct to refers the team that can work in a residency
 type StaffMember struct {
-	ID          bson.ObjectId `bson:"_Id"           json:"Id"`
-	Name        string        `bson:"name"          json:"name"`
-	Email       string        `bson:"email"         json:"email"`
-	Photo       int64         `bson:"photo"         json:"photo"`
-	Core        bool          `bson:"isCore"        json:"isCore"`
-	ResidencyID string        `bson:"residencyId"   json:"residencyId"`
+	ID           bson.ObjectId `bson:"_Id"           	json:"Id"`
+	Name         string        `bson:"name"          	json:"name"`
+	Email        string        `bson:"email"         	json:"email"`
+	Photo        int64         `bson:"photo"         	json:"photo"`
+	IsCoreMember bool          `bson:"isCoreMember"		json:"isCoreMember"`
+	ResidencyID  string        `bson:"residencyId"   	json:"residencyId"`
 }
